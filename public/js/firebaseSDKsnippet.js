f892527358dfd46b326d4ff7d06aba05b94e6629
antenmanuuel01 <84549979+antenmanuuel01@users.noreply.github.com>
2021-12-06 11:25:56 -0500

    Initial Commit

---
+++ public/js/firebaseSDKsnippet.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+      apiKey: "AIzaSyBAeKIcaQG9NERrivK-CjJJ49V7LwR6hvo",
+      authDomain: "bcs240-a8cae.firebaseapp.com",
+      projectId: "bcs240-a8cae",
+      storageBucket: "bcs240-a8cae.appspot.com",
+      messagingSenderId: "567192564823",
+      appId: "1:567192564823:web:198f826b6527d19ef41f12",
+      measurementId: "G-NHZRB67EX3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
